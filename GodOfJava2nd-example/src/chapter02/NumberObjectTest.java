49781c6180226845885e744abcf7f885ee2e544d
naeunJeon <mika94322@gmail.com>
2018-08-07 21:48:05 +0900

    Add Junit test code

--- GodOfJava2nd-example/src/chapter02/NumberObjectTest.java
+++ GodOfJava2nd-example/src/chapter02/NumberObjectTest.java
@@ -3,6 +3,7 @@ package chapter02;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.List;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,11 @@ class NumberObjectTest {
 		NumberObject num = new NumberObject();
 		assertEquals(-1, num.parseLong("r1024"));
 		assertEquals(1024, num.parseLong("1024"));
+		
+		String[] strings = {"10000000000", "400", "2000"};
+		assertArrayEquals(strings, num.printOtherBase(1024));
 	}
+
 }
+
 
